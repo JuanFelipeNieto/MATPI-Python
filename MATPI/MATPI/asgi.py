a70080b6311c0e51e261bd851d9ab2e42bfac7ba
JuanNieto3001 <juanfenietoc@gmail.com>
2026-03-05 08:27:25 -0500

    commit

---
+++ MATPI/MATPI/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for MATPI project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MATPI.settings')
+
+application = get_asgi_application()
